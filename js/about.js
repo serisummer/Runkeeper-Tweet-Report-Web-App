c8aade573fe038a9c565239b6b09dacacbc1d856
Weiguang Wu <wwg12121@gmail.com>
2023-10-22 23:47:32 -0700

    Part 1: Tweet Dates, Tweet Categories

--- js/about.js
+++ js/about.js
@@ -11,7 +11,61 @@ function parseTweets(runkeeper_tweets) {
 	
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
-	document.getElementById('numberTweets').innerText = tweet_array.length;	
+	document.getElementById('numberTweets').innerText = tweet_array.length;
+	const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+	const firstDate = tweet_array[tweet_array.length-1].time.toLocaleDateString(undefined, options);
+	const lastDate = tweet_array[0].time.toLocaleDateString(undefined, options);
+	document.getElementById('firstDate').innerText = firstDate;
+	document.getElementById('lastDate').innerText = lastDate;
+
+	let completed = 0, live = 0, achieve = 0, mis = 0;
+	tweet_array.forEach(element => {
+		let tweet = new Tweet(element.text, element.time);
+		if (tweet.source == 'completed_event') {
+			completed += 1;
+		}
+		else if (tweet.source == 'live_event') {
+			live += 1;
+		}
+		else if (tweet.source == 'achievement') {
+			achieve += 1;
+		}
+		else {
+			mis += 1;
+		}
+	});
+	
+	let completedPct = math.format(completed / tweet_array.length * 100, 3),
+		livePct = math.format(live / tweet_array.length * 100, 3),
+		achievePct = math.format(achieve / tweet_array.length * 100, 3),
+		misPct = math.format(mis / tweet_array.length * 100, 3);
+
+	Array.from(document.getElementsByClassName('completedEvents')).forEach(element => {
+		element.innerText = completed;
+	});
+	Array.from(document.getElementsByClassName('liveEvents')).forEach(element => {
+		element.innerText = live;
+	});
+	Array.from(document.getElementsByClassName('achievements')).forEach(element => {
+		element.innerText = achieve;
+	});
+	Array.from(document.getElementsByClassName('miscellaneous')).forEach(element => {
+		element.innerText = mis;
+	});
+
+
+	Array.from(document.getElementsByClassName('completedEventsPct')).forEach(element => {
+		element.innerText = completedPct + '%';
+	});
+	Array.from(document.getElementsByClassName('liveEventsPct')).forEach(element => {
+		element.innerText = livePct + '%';
+	});
+	Array.from(document.getElementsByClassName('achievementsPct')).forEach(element => {
+		element.innerText = achievePct + '%';
+	});
+	Array.from(document.getElementsByClassName('miscellaneousPct')).forEach(element => {
+		element.innerText = misPct + '%';
+	});
 }
 
 //Wait for the DOM to load
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@
 1. How many assignment points do you believe you completed (replace the *'s with your numbers)?
 
 */20
-- */2 Tweet dates
-- */3 Tweet categories
+- 2/2 Tweet dates
+- 3/3 Tweet categories
 - */3 User-written tweets
 - */3 Determining activity type and distance
 - */3 Graphing activities by distance
@@ -12,16 +12,17 @@
 - */3 Populating the table
 
 2. How long, in hours, did it take you to complete this assignment?
-
+3 hours
 
 
 3. What online resources did you consult when completing this assignment? (list sites like StackOverflow or specific URLs for tutorials, etc.)
-
-
+https://mathjs.org/docs/reference/functions/format.html
+https://stackoverflow.com/questions/3871547/iterating-over-result-of-getelementsbyclassname-using-array-foreach
+https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByClassName
 
 4. What classmates or other individuals did you consult as part of this assignment? What did you discuss?
-
+None
 
 
 5. Is there anything special we need to know in order to run your code?
-
+None
